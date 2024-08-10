630ab76d4fc32391010bbe314f0f8ab35d4a3938
tinhthanh <k40cntt@gmail.com>
2024-08-10 16:21:58 +0700

    add ws

---
+++ src/views/portal/services/websocket.service.ts
@@ -0,0 +1,7 @@
+import { VgWebSocketService } from 'vg-table-sdk';
+import { Injectable } from '../../../utils/inject';
+@Injectable
+export class WebSocketService  extends VgWebSocketService {
+
+    
+}
